3a33f4e8b42af76cc7d452794ec0e16f41a80ac0
Abhishek Sharma <abhi761685@gmail.com>
2026-08-03 17:31:19 +0530

    feat: create auth route

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import helmet from "helmet";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { errorHandler } from "./middlewares/error.middleware.js";
+import authRoutes from "./modules/auth/auth.routes.js"
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.use(helmet());
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
+
+app.use("/api/v1/auth", authRoutes)
 
 app.get("/health", (_, res) => {
   res.json({
