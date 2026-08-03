ec70486327e090e7842d7ecd226917e1e11d5e6d
Abhishek Sharma <abhi761685@gmail.com>
2026-08-03 16:31:02 +0530

    feat: setup authentication module

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,15 +1,23 @@
 import type { NextFunction, Request, Response } from "express";
+import { AppError } from "../common/errors/AppError.js";
 
 export function errorHandler(
-  err: Error,
-  req: Request,
-  res: Response,
-  next: NextFunction
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
 ) {
-  console.error(err);
+    if (err instanceof AppError) {
+        return res.status(err.statusCode).json({
+            success: false,
+            message: err.message,
+        });
+    }
 
-  return res.status(500).json({
-    success: false,
-    message: "Internal Server Error",
-  });
+    console.error(err);
+
+    return res.status(500).json({
+        success: false,
+        message: "Internal Server Error",
+    });
 }
