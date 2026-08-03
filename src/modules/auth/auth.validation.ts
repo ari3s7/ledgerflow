2fa4a5f6ffa8180c630ed3fd6b86e5a41cbad1f0
Abhishek Sharma <abhi761685@gmail.com>
2026-08-03 18:21:43 +0530

    feat: add login zod schema

--- src/modules/auth/auth.validation.ts
+++ src/modules/auth/auth.validation.ts
@@ -4,6 +4,12 @@ export const registerSchema = z.object({
     name: z.string().min(3),
     email: z.email(),
     password: z.string().min(8),
-})
+});
+
+export const loginSchema = z.object({
+    email: z.email(),
+    password: z.string().min(8)
+});
 
 export type RegisterInput = z.infer<typeof registerSchema>
+export type LoginInput = z.infer<typeof loginSchema>
